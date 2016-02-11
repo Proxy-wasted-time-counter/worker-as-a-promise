3a8c8d04569e3bde48c70390ba082662cda1e22e
kepennar <kevin.pennarun@gmail.com>
2016-02-11 16:21:42 +0100

    First useable version

--- examples/echo-worker.js
+++ examples/echo-worker.js
@@ -1,5 +1,25 @@
-onmessage = function(e) {
-  const timeout = (Math.random() * 3000) + 1;
-  setTimeout(() => postMessage(e.data), timeout);
-};
+import { isMessageAsPromised, getMessage, createResponse, createError } from '../src';
 
+self.addEventListener('message', m => {
+  if (isMessageAsPromised(m)) {
+    echoAsPromised(m);
+  } else {
+    echo(m);
+  }
+});
+
+function echo(m) {
+  const timeout = (Math.random() * 3000) + 1;
+
+  console.log('[WORKER] - This is not a message as promised :(', m.data);
+  setTimeout(() => self.postMessage({message: m.data.message}), timeout);
+}
+
+function echoAsPromised(m) {
+  const timeout = (Math.random() * 3000) + 1;
+
+  const message = getMessage(m);
+  console.log('[WORKER] - I got the message', message);
+  const response = Math.random() > .5 ? createResponse(message) : createError(message);
+  setTimeout(() => self.postMessage(response), timeout);
+}
--- examples/index.js
+++ examples/index.js
@@ -1,10 +1,16 @@
-import workerAsPromise from '../src';
+import { decorateWorker } from '../src';
 
 import EchoWorker from 'worker!./echo-worker';
 
-const worker = workerAsPromise(new EchoWorker());
+const worker = decorateWorker(new EchoWorker());
 
 for (let i = 0; i < 5; i++) {
-  worker.sendMessage('test', {data: {message: `message ${i}`}})
-    .then(response =>console.log(`response ${i}`, response));
+  worker.sendMessage({message: `message ${i}`})
+    .then(response => console.log(`[MAIN] - Response ${i}`, response))
+    .catch(responseError => console.error(`[MAIN] - Response ${i}`, responseError));
+
+  worker.postMessage({message: `Classical message ${i}`});
 }
+worker.addMessageListener(data => {
+  console.log('[MAIN] - Classical response', data);
+});
